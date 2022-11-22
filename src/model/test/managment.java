5db08cc13ce4884de0528aa6e5b42e48d0200b56
Muhammad Mubashir Mughal <donleo26@gmail.com>
2022-11-22 03:08:02 -0500

    managment

--- src/model/test/managment.java
+++ src/model/test/managment.java
@@ -1,11 +1,24 @@
 package model.test;
 
 import model.employee.Employee;
+import model.vehicle.Motorcycle;
+import model.vehicle.Vehicle;
 
 import java.lang.reflect.Type;
 
-public class managment {
-    static double workingDayPerMonth = 20.0;
+public class managment extends Motorcycle{
+    static double workingDaysPerMonth = 20.0;
+
+    public managment(String make, String plate, String color, String category, boolean sideCar) {
+        super(make, plate, color, category, sideCar);
+    }
+
+    //"Kawasaki", "Custom Plate", "Yellow", "Race Motorcycle", false
+    public void motorcycle(String make, String plate, String color, String category, boolean sideCar){
+        Vehicle.setMake("Kawasaki");
+        Vehicle.setPlate("Custom Plate");
+        System.out.println();
+    }
 
    // Type employee = [Employee]();
 
