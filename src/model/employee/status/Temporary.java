7809afe1a362274a527228ec536202991e0124f6
Muhammad Mubashir Mughal <donleo26@gmail.com>
2022-11-22 01:49:25 -0500

    Temporary

--- src/model/employee/status/Temporary.java
+++ src/model/employee/status/Temporary.java
@@ -1,4 +1,21 @@
 package model.employee.status;
 
 public class Temporary {
+    private double hourlySalary;
+    private int accumulatedHours;
+
+    public Temporary (double hourlySalary, int accumulatedHours) {
+        this.hourlySalary = hourlySalary;
+        this.accumulatedHours = accumulatedHours;
+    }
+
+    public double getHourlySalary(){
+        return this.hourlySalary;
+    }
+    public int getAccumulatedHours(){
+        return this.accumulatedHours;
+    }
+    public double accumulatedSalary(){
+        return hourlySalary * accumulatedHours * 12;
+    }
 }
